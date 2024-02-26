40041c1a000ff055b5d541537fd0670bc90f70ab
Daniil <danilmux@gmail.com>
2024-02-26 23:17:44 +0500

    Strict mode

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const elem = <h2>Hello World</h2>;
-
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-    <h2 className='text'>Текст: {text}</h2>
-    <input type="text" />
-    <label htmlFor=""></label>
-    <button tabIndex="0">click</button>
-  </div>
-);
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
+
