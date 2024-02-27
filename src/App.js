8dfeb0c436b1061773828da3ac6d064968a68cdf
Daniil <danilmux@gmail.com>
2024-02-27 20:42:03 +0500

    component's props

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
-import { Component, StrictMode } from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello World!</h2>;
-};
-
-// const Field = () => {
-//   const holder = 'Enter here';
-//   const styledField = {
-//     width: '300px'
-//   };
-//   return <input
-//     placeholder={holder}
-//     type='text'
-//     style={styledField} />;
-// };
-
-class Field extends Component {
-  render() {
-    const holder = 'Enter here';
-    const styledField = {
-      width: '300px'
-    };
-
-    return <input
-      placeholder={holder}
-      type='text'
-      style={styledField} />;
-  }
-}
-
-function Btn() {
-  const text = 'Log in';
-  const logged = false;
-
-  return <button>{logged ? 'Enter' : text}</button>;
+function WhoAmI({name, surname, link}) {
+  return (
+    <div>
+      <h1>My name is {name()}, surname - {surname}</h1>
+      <a href={link}>My profile</a>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <StrictMode>
-        <Header />
-      </StrictMode>
-      <Field />
-      <Btn />
+      <WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"  />
+      <WhoAmI name={() => {return 'John'}} surname="Shepard" link="vk.com"  />
     </div>
   );
 }
 
-export { Header };
 export default App;
